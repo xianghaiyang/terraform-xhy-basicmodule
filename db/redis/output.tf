8da890e07cedeedab1868979489bf7d733281ccf
sfadf <sadfsadf>
2021-01-27 15:20:16 +0800

    重置仓库

--- db/redis/output.tf
+++
@@ -1,8 +0,0 @@
-output "instance_ids" {
-  value = "${alicloud_kvstore_instance.default.*.id}"
-}
-
-# 输出redis的连接地址
-output "connection_domain" {
-  value = "${alicloud_kvstore_instance.default.*.connection_domain}"
-}
